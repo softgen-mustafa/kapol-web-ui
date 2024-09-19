ee94fedd74aecd8cb09555c851f260252ca2283f
Khan Mustafa <146811268+softgen-mustafa@users.noreply.github.com>
2024-09-19 16:39:18 +0530

    Inprogress: dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { theme } from "./theme";
+import theme from "./theme";
 
 export const metadata: Metadata = {
   title: "Kapol App",
--- app/theme.ts
+++ app/theme.ts
@@ -2,9 +2,10 @@ import { createTheme } from "@mui/material";
 import "@fontsource/dm-sans";
 
 const theme = createTheme({
+  palette: {},
   typography: {
     fontFamily: "DM Sans",
   },
 });
 
-export { theme };
+export default theme;
