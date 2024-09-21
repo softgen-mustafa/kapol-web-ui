ac1a3a6a2c99f7de4f92c6d19fc18c633218f0a2
Khan Mustafa <146811268+softgen-mustafa@users.noreply.github.com>
2024-09-21 18:15:11 +0530

    Done: Register Integration

--- app/assets/images.ts
+++ app/assets/images.ts
@@ -1,8 +1,5 @@
 export const images = {
   job: require("./job.png"),
-<<<<<<< HEAD
-  rohit: require("./385819.webp")
-=======
+  rohit: require("./385819.webp"),
   companyLogo: require("./company_logo.png"),
->>>>>>> ee7cb0dbbd6be23ca545fd7ecc6a15320a2213a1
 };
