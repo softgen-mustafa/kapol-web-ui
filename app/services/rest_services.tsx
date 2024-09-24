2fa122ee27c5d6b4fecdf609da32513027e7c9bb
Khan Mustafa <146811268+softgen-mustafa@users.noreply.github.com>
2024-09-24 18:07:05 +0530

    Done: User Image

--- app/services/rest_services.tsx
+++ app/services/rest_services.tsx
@@ -96,7 +96,7 @@ const getAsync = async (url: string) => {
 
 const getBaseUrl = () => {
   //Local
-  return "http://localhost:45001";
+  return "http://192.168.1.18:45001";
   // return "https://softgensolutions.in/service";
 };
 
