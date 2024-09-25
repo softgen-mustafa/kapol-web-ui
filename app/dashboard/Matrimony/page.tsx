02a9cee68481d8e65a6b5096c45baf10d44227b5
Khan Mustafa <146811268+softgen-mustafa@users.noreply.github.com>
2024-09-25 16:38:34 +0530

    Inprogress: Matrimony Profile

--- app/dashboard/Matrimony/page.tsx
+++ app/dashboard/Matrimony/page.tsx
@@ -1,20 +1,36 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import { Box, Grid2, Typography } from "@mui/material";
 import ProfileCard, { Profiles } from "@/app/components/matrimony";
-
+import { getAsync, getBaseUrl } from "@/app/services/rest_services";
 
 const Matrimony = () => {
-  const [profileList, setProfileList] = useState(Profiles); // Set initial state with dummy data
+  const [profileList, setProfileList] = useState(Profiles);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    try {
+      let url = `${getBaseUrl()}/matrimony/get?gender=male&status=all`;
+
+      const response = await getAsync(url);
+
+      console.log("Response:", response);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
 
   return (
     <Box sx={{ padding: 1.8, backgroundColor: "#f5f5f5", minHeight: "100vh" }}>
-      <Typography variant="h5"  sx={{ marginBottom: 4, color: "#232325" }}>
+      <Typography variant="h5" sx={{ marginBottom: 4, color: "#232325" }}>
         Profile Wall
       </Typography>
       <Grid2 container spacing={4} justifyContent="center">
         {profileList.map((profile) => (
-          <Grid2 item xs={12} sm={3} md={6} key={profile.id}>
+          <Grid2 size={{ xs: 12, sm: 3, md: 6 }} key={profile.id}>
             <ProfileCard
               image={profile.image}
               name={profile.name}
@@ -25,13 +41,15 @@ const Matrimony = () => {
               caste={profile.caste}
               education={profile.education}
               occupation={profile.occupation}
-              sx={{
-                transition: "0.3s",
-                "&:hover": {
-                  boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.2)",
-                  transform: "scale(1.02)",
-                },
-              }}
+              id={0}
+              bio={""}
+              // sx={{
+              //   transition: "0.3s",
+              //   "&:hover": {
+              //     boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.2)",
+              //     transform: "scale(1.02)",
+              //   },
+              // }}
             />
           </Grid2>
         ))}
