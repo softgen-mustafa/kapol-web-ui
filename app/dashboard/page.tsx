43ea108d6f55f725af2d6bd9ed5fd914a4bc0125
softgenaditya <softgen.aditya.pal@gmail.com>
2024-10-26 17:13:23 +0530

    dashboard changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -81,6 +81,22 @@ const Page = () => {
             onClick={() => router.push("/dashboard/socialwar")}
           />
         </Grid2>
+        <Grid2 size={{ md: 6, sm: 6, xs: 12 }}>
+          <Card
+            description="Finding true love in todays fast-paced world can be challenging. But sometimes, the most beautiful stories unfold in the most unexpected ways. My sisters journey to finding her soulmate is one such story, and it all began with Shaadi.com."
+            title="Schemes"
+            imageSrc={socialWar}
+            onClick={() => router.push("")}
+          />
+        </Grid2>
+        <Grid2 size={{ md: 6, sm: 6, xs: 12 }}>
+          <Card
+            title="About US"
+            description="Finding true love in todays fast-paced world can be challenging. But sometimes, the most beautiful stories unfold in the most unexpected ways. My sisters journey to finding her soulmate is one such story, and it all began with Shaadi.com."
+            imageSrc={culture}
+            onClick={() => router.push("")}
+          />
+        </Grid2>
       </Grid2>
     </Box>
   );
