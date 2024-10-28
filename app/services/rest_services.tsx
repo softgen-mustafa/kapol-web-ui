dba2ea108bcea9c9ddc4a50bc187c812e2860d29
softgen-prajyot <prajyot.wankhade.softgen@gmail.com>
2024-10-28 17:18:27 +0530

    added the ip

--- app/services/rest_services.tsx
+++ app/services/rest_services.tsx
@@ -102,7 +102,7 @@ const deleteAsync = async (url: string) => {
 
 const getBaseUrl = () => {
   //Local
-  return "http://192.168.1.20:45001";
+  return "http://192.168.1.7:45001";
   // return "https://softgensolutions.in/service";
 };
 
--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ module.exports = {
         remotePatterns: [
             {
                 protocol: 'http',
-                hostname: '192.168.1.20',  // The IP address or domain of your image source
-                port: '45001',             // The port where your backend is serving the images
-                pathname: '/imageservice/image/**',  // Path pattern for the image service
+                hostname: '192.168.1.7', // The IP address or domain of your image source
+                port: '45001', // The port where your backend is serving the images
+                pathname: "/imageservice/image/**", // Path pattern for the image service
             },
         ],
     },
