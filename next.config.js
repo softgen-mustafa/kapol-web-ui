ebec46552950d0755e75cc13902fffdd6ad62e0c
softgen-mustafa <softgen.mustafa.khan@gmail.com>
2024-10-28 17:13:00 +0530

    added the ip

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 // next.config.js
 module.exports = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: '192.168.1.20',  // The IP address or domain of your image source
-                port: '45001',             // The port where your backend is serving the images
-                pathname: '/imageservice/image/**',  // Path pattern for the image service
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "192.168.1.7", // The IP address or domain of your image source
+        port: "45001", // The port where your backend is serving the images
+        pathname: "/imageservice/image/**", // Path pattern for the image service
+      },
+    ],
+  },
 };
