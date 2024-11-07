e41ef99e1760716790688026bd8f7b4d4416f18a
softgen-prajyot <prajyot.wankhade.softgen@gmail.com>
2024-11-07 15:56:14 +0530

    UI Level Changes Related to Matrimony

--- app/dashboard/jobportal/[jobdetails]/page.tsx
+++ app/dashboard/jobportal/[jobdetails]/page.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import { images } from "@/app/assets/images";
 import { getAsync, getBaseUrl, postAsync } from "@/app/services/rest_services";
 import { fetchCurrentUser } from "@/app/services/Local/helper";
+import theme from "@/app/theme";
 
 interface JobDetails {
   ID: string;
@@ -66,7 +67,8 @@ const JobPage = ({ params }: { params: any }) => {
     <Box
       p={2}
       sx={{
-        backgroundColor: "#FFF8F0",
+        backgroundColor: theme.palette.customColors.parchment,
+        minHeight: "100vh",
       }}
     >
       <Typography
@@ -82,21 +84,42 @@ const JobPage = ({ params }: { params: any }) => {
       {/* <Typography fontSize={30} fontWeight={"600"} color="#232325">
         Job Details
       </Typography> */}
-      <Box p={3} mt={1} className="bg-white" sx={{ borderRadius: 3 }}>
+      <Box
+        p={3}
+        mt={1}
+        sx={{
+          borderRadius: 3,
+          bgcolor: theme.palette.customColors.parchmentLight2,
+          border: `2px solid ${theme.palette.customColors.goldenrod}`,
+        }}
+      >
         <Image
           src={images.companyLogo}
           alt="Company Logo"
           width={45}
           height={45}
+          style={{
+            border: `1.5px solid ${theme.palette.customColors.goldenrod}`,
+          }}
+          className=" rounded-xl shadow-md transition-shadow duration-300 ease-in-out hover:shadow-lg"
         />
         <Stack spacing={1} mt={0.5}>
-          <Typography color="#232325" fontSize={22} fontWeight={"600"}>
+          <Typography
+            fontSize={22}
+            fontWeight={"600"}
+            sx={{ color: theme.palette.primary.light }}
+          >
             {jobDetails.Position}
           </Typography>
           <Box className="flex flex-col items-start gap-1 mb-2">
             <Box className="flex flex-row items-center gap-1">
-              <BusinessIcon sx={{ color: "#376fd0" }} />
-              <Typography color="#376fd0" fontSize={18}>
+              <BusinessIcon
+                sx={{ color: theme.palette.customColors.antiqueGold }}
+              />
+              <Typography
+                fontSize={18}
+                sx={{ color: theme.palette.customColors.goldenrod }}
+              >
                 {jobDetails.CompanyName}
               </Typography>
             </Box>
@@ -130,11 +153,11 @@ const JobPage = ({ params }: { params: any }) => {
               px: 3,
               py: 1,
               borderRadius: "12px",
-              background: "linear-gradient(45deg, #04A7E5 30%, #1E90FF 90%)",
+              background: "linear-gradient(45deg, #FFA726 30%, #FF7043 90%)",
               boxShadow:
                 "0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)",
               "&:hover": {
-                background: "linear-gradient(45deg, #1E90FF 30%, #00BFFF 90%)",
+                background: "linear-gradient(45deg, #FF8C00 30%, #FFA500 90%)",
                 boxShadow: "0px 4px 6px -2px rgba(0,0,0,0.3)",
               },
               textTransform: "capitalize",
@@ -147,7 +170,11 @@ const JobPage = ({ params }: { params: any }) => {
         <Divider sx={{ my: 2.5 }} />
         <Stack flexDirection={"column"} gap={2}>
           <Box className="flex flex-col gap-1">
-            <Typography color="#232325" fontSize={18} fontWeight={"600"}>
+            <Typography
+              fontSize={18}
+              fontWeight={"600"}
+              sx={{ color: theme.palette.primary.light }}
+            >
               Job description
             </Typography>
             <Typography color="#666666" fontSize={16}>
